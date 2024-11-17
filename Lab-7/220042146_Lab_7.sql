5123b9584f39e6a3d0a1fa72b2f914e3a32c5c4c
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-11-17 15:01:32 +0600

    added Lab_7 files

---
+++ Lab-7/220042146_Lab_7.sql
@@ -0,0 +1,50 @@
+-- Create table for Research Group
+CREATE TABLE Research_Group (
+    Group_Name VARCHAR(100) PRIMARY KEY,
+    Dedicated_Lab VARCHAR(100),
+    Budget DECIMAL(10, 2),
+    Group_Head VARCHAR(100)
+);
+
+-- Create table for Faculty
+CREATE TABLE Faculty (
+    Faculty_ID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Contact_Number VARCHAR(15),
+    Group_Name VARCHAR(100),
+    FOREIGN KEY (Group_Name) REFERENCES Research_Group(Group_Name)
+);
+
+-- Create table for Student
+CREATE TABLE Student (
+    Student_ID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Contact_Number VARCHAR(15),
+    Group_Name VARCHAR(100),
+    Supervisor INT,
+    FOREIGN KEY (Group_Name) REFERENCES Research_Group(Group_Name),
+    FOREIGN KEY (Supervisor) REFERENCES Faculty(Faculty_ID)
+);
+
+-- Create table for Project
+CREATE TABLE Project (
+    Project_Title VARCHAR(100) PRIMARY KEY,
+    Start_Date DATE,
+    Estimated_End_Date DATE,
+    Research_Domain VARCHAR(100),
+    Supervisor INT,
+    FOREIGN KEY (Supervisor) REFERENCES Faculty(Faculty_ID)
+);
+
+
+
+-- Create table for Publications
+CREATE TABLE Publication (
+    Paper_Title VARCHAR(100) PRIMARY KEY,
+    Conference_Name VARCHAR(100),
+    Publication_Date DATE,
+    Research_Domain VARCHAR(100),
+    Summary TEXT,
+    Group_Name VARCHAR(100),
+    FOREIGN KEY (Group_Name) REFERENCES Research_Group(Group_Name)
+);
